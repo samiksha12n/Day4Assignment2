956ff4889bd9b6814d10939aecd119ff9ee56b43
samiksha12n <samikshaunesarkar@gmail.com>
2023-11-09 15:11:03 +0530

    Using Loop

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4Part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The first 10 natural numbers are as follows\n");
+            for (int i = 0;i<=10;i++)
+            {   
+                Console.WriteLine("{0}",i);
+            }
+            Console.WriteLine("\n");
+            int n = 0;
+            Console.WriteLine("Even numbers are as follows\n");
+            while (n<=10)
+            {   
+                Console.WriteLine(n);
+                n = n + 2;
+            }
+            int num=0;
+            Console.WriteLine("The odd numbers");
+            do
+            {
+                num = num + 1;
+                Console.WriteLine(num);
+            }while(num<=10);
+            Console.ReadKey();
+        }
+        
+    }
+}
